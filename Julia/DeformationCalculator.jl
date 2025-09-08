1e78cd80310e16107123ce6c8a1c0b974ea302db
Miguel Madurga Flores <miguel.madurga@gmail.com>
2025-09-08 11:29:29 -0400

    cleaned up DeformationCalculator

--- Julia/DeformationCalculator.jl
+++ Julia/DeformationCalculator.jl
@@ -15,18 +15,32 @@ function qo(BE2,CG2)
     return sqrt( 16π/5 * BE2/CG2 )
 end
 
+"""
+
+beta2odd(A,Z,Ji,Jf,K,BE2)
+
+Deformation calculated for the B(E2) between Ji and Jf members of band K.
+
+
+"""
 function beta2odd(A,Z,Ji,Jf,K,BE2)
     R=1.2*A^(1/3)
     CG=cg2(Ji,Jf,K)
     Q=qo(BE2,CG)
-    return sqrt( 5π)/3 * Q/(Z*R^2 )
+    return sqrt(5π)/3 * Q/(Z*R^2)
 end
 
-function beta2even( A::Int,  Z::Int, BE2::Float64)
+
+"""
+
+beta2even(A,Z,BE2↑)
+
+β₂ calculator for B(E2 0⁺→2⁺). For B(E2 2⁺→0⁺) multiply BE2↓ by 5.
+
+"""
+function beta2even( A::Int,  Z::Int, BE2::Any) 
     R = 1.2 * A^(1/3)
     return (4π) / (3 * Z * R^2) * sqrt(BE2)
 end
-
-
 
 end
